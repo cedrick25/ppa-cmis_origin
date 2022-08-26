842b35a3ed12edecf253cbc7da05ae82f1ff5409
Hansel Mejarito <mejarito.hansel@gmail.com>
2022-08-26 13:54:17 +0800

    updated

--- application/views/login.php
+++ application/views/login.php
@@ -8,7 +8,7 @@
 	<div class="row" >
 		<div class="col-md-12" >
 				<div class="panel panel-default">
-					 <div class="panel-body" >
+					 <div class="panel-body" style="background-image: url('assets/img/bg.jpg');height:450px">
 					 	<div class="col-md-3">
 							</div>		
 
