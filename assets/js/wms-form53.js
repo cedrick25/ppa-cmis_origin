8a2fbd5a29d62720ec8853ec1663353a78472725
hansel <mejarito.hansel@gmail.com>
2023-05-02 10:39:17 +0800

    updated

--- assets/js/wms-form53.js
+++ assets/js/wms-form53.js
@@ -829,7 +829,7 @@ $.wms.form53 = (function() {
                     "<td class='options field'>"+data.fieldOffice+"</td>"+
                     "<td class='options'>"+source+"</td>"+
                     "<td align='center' class='options'> <button class='access_f53_write btn btn-success btn-sm btn-edit form_lock' data-docket='"+data.docketNumber.toUpperCase()+"' data-id='"+data.id+"'><i class='fa fa-pencil'></i> Update</button> "+
-                    "<button class='access_f53_write btn btn-danger btn-sm btn-delete hidden form_lock' data-docket='"+data.docketNumber.toUpperCase()+"' data-id='"+data.id+"'><i class='fa fa-trash'></i> Delete</button> </td></tr>")
+                    "<button class='access_f53_write btn btn-danger btn-sm btn-delete form_lock' data-docket='"+data.docketNumber.toUpperCase()+"' data-id='"+data.id+"'><i class='fa fa-trash'></i> Delete</button> </td></tr>")
             });
             $(document).ready(function () {
                 var table = $('#T_F53T3').DataTable({
