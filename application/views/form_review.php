48aadc5db25b54502a9010cccaf787e2e302e014
hansel <mejarito.hansel@gmail.com>
2023-06-01 11:24:04 +0800

    updated

--- application/views/form_review.php
+++ application/views/form_review.php
@@ -44,7 +44,7 @@
         </div>
         <div class="modal-body">
             <div class="alert alert-success">
-              <strong>Processing!</strong> Please wait.<i class="fa fa-refresh fa-spin fa-1x fa-fw spin"></i></span>
+              <strong>Processing!</strong> "Thank you for your patience. We are currently processing your carryover request. Please note that this may take some time due to high demand. We apologize for the delay and appreciate your understanding. If you have any urgent concerns, please feel free to reach out to our customer support team."<i class="fa fa-refresh fa-spin fa-1x fa-fw spin"></i></span>
             </div>
         </div>
       </div>
