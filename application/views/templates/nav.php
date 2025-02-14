83c85bd68391b7f70889227d9c492fec6d3c1307
Hansel mejarito <mejarito.hansel@gmail.com>
2025-02-14 11:24:16 +0800

    updates

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -125,9 +125,9 @@
                                
                                 <li><a href="probationer" class="">Probation Masterlist</a></li>
                                 <li><a href="probationer_approval" class="">Probation For Approval</a></li> 
-                                <li><a href="community_service_masterlist" class="demo_purpose hidden">Community Service Masterlist</a></li> 
-                                <li><a href="no_reports" class="demo_purpose hidden">No Submitted Reports in Probation Caseload</a></li> 
-                                <li><a href="no_submitted_reports_community" class="demo_purpose hidden">No Submitted Reports in Community Service</a></li> 
+                                <li><a href="community_service_masterlist" class="access_csm">Community Service Masterlist</a></li> 
+                                <li><a href="no_reports" class="access_pc_exception">No Form 5 Caseload Reports Submitted</a></li> 
+                                <li><a href="no_submitted_reports_community" class="demo_purpose hidden">No Form 53 Caseload Reports Submitted</a></li> 
                             </ul>
                         </li>
 
