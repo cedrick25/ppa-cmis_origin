71c6b775af6ef1ae21ced06cc8141dd6f2cae617
Hansel <mejarito.hansel@gmail.com>
2026-04-17 10:57:30 +0800

    reports result enye done

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -33,9 +33,10 @@ class Report extends CI_Controller {
         $fname  = $filters['FIRSTNAME'];
         $mname  = $filters['MIDDLENAME'];
         $lname  = $filters['LASTNAME'];
-
         $full_name = $fname . ' ' . $mname . ' ' . $lname;
-        $fullname = ucwords(strtolower($full_name));
+
+        // Use mb_convert_case for proper multi-byte (UTF-8) support, handles ñ correctly
+        $fullname = mb_convert_case(mb_strtolower($full_name, 'UTF-8'), MB_CASE_TITLE, 'UTF-8');
         // Send request to API for masterlist
         $apiUrl_json = 'http://localhost/ppa-cmis-api_origin/wsv1/Cmis/masterlist_json';
         $ch = curl_init($apiUrl_json);
@@ -257,13 +258,13 @@ class Report extends CI_Controller {
             'end_date'        => $this->input->post('end_date'),   // expect YYYY-MM-DD
         ];
 
-        // Extract region and year for display
-        $fname  = $filters['first_name'];
-        $mname  = $filters['middle_name'];
-        $lname  = $filters['last_name'];
-
+        $fname  = $filters['FIRSTNAME'];
+        $mname  = $filters['MIDDLENAME'];
+        $lname  = $filters['LASTNAME'];
         $full_name = $fname . ' ' . $mname . ' ' . $lname;
-        $fullname = ucwords(strtolower($full_name));
+
+        // Use mb_convert_case for proper multi-byte (UTF-8) support, handles ñ correctly
+        $fullname = mb_convert_case(mb_strtolower($full_name, 'UTF-8'), MB_CASE_TITLE, 'UTF-8');
         // Send request to API for community_masterlist
         $apiUrl_json = 'http://localhost/ppa-cmis-api_origin/wsv1/Expansion/community_json';
         $ch = curl_init($apiUrl_json);
